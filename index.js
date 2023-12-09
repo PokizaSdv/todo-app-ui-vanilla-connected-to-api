496785bf19e0df76965c4449fd1d7ae51888f3e6
PokizaSdv <pokizasdv@gmail.com>
2023-12-09 02:39:21 -0500

    Create Todo API

---
+++ index.js
@@ -0,0 +1,75 @@
+class TodoAPI {
+    async create(text) {
+        try {
+            const response = await fetch("http://localhost:4000/tasks", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    text: text
+                })
+            });
+
+            if (!response.ok) {
+                const data = await response.json();
+                throw new Error(data.message);
+            }
+
+            return await response.json();
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+    async getAll() {
+        try {
+            const response = await fetch("http://localhost:4000/tasks");
+            if (!response.ok) {
+                throw new Error(response.error);
+            }
+
+            return await response.json();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async deleteOne(id) {
+        try {
+            const response = await fetch(`http://localhost:4000/tasks/${id}`, {
+                method: "DELETE"
+            });
+
+            if (!response.ok) {
+                const data = await response.json();
+                throw new Error(data.message);
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    async updateStatus(id, status) {
+        try {
+            const response = await fetch(`http://localhost:4000/tasks/${id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    status: status
+                })
+            });
+
+            if (!response.ok) {
+                const data = await response.json();
+                throw new Error(data.message);
+            }
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+}
+
+const todoAPI = new TodoAPI();
+
